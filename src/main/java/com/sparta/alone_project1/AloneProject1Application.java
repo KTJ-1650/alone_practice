a4c5300c6c3d59826b74153ed7f850bb84321297
KTJ-1650 <xowns1650@gmail.com>
2024-10-01 08:46:09 +0900

    first commit

---
+++ src/main/java/com/sparta/alone_project1/AloneProject1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.alone_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloneProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AloneProject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/alone_project1/AloneProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.alone_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloneProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
